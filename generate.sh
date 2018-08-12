25a5f9157b89a5c5deb975681249b2983353f1ca
Pierre-Hugues Husson <phh@phh.me>
2018-08-12 21:25:32 +0200

    Add generate.sh to create bp/makefiles

---
+++ generate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+find vendor/interfaces -type d -name '[0-9].[0-9]' |while read intf;do
+	interface="$(echo $intf |sed -E 's;.*vendor/interfaces/(.*)/([0-9.]*).*;\1;g' | tr '/' '.')"
+	version="$(  echo $intf |sed -E 's;.*vendor/interfaces/(.*)/([0-9.]*).*;\2;g')"
+	full="${interface}@${version}"
+	for target in androidbp makefile;do
+		hidl-gen \
+			-r android.hardware:hardware/interfaces \
+			-r vendor:vendor/interfaces/vendor \
+			-L "$target" \
+			$full
+	done
+done
